1801769b1d792d41ca73ad23194446c4132dd696
David Yackzan <dwyackzan@gmail.com>
2023-01-18 15:24:52 -0500

    Add checks and packages
    Add checks before attempting to install and clone oh-my-zsh files as the
    script throws errors upon re-install attempt.
    
    Install some pip packages I'll pretty much always want.

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
+#!/bin/bash
+
 # Create an install script to install all packages
 # another cool way to do this would be to build a Docker container for use on different OS
 # but for this case I will make this script assuming ubuntu is installed
 
-# 1. install tools
-sudo apt install -y git zsh tmux neovim curl npm python3-pip ranger
+# 1. Update and install tools
+sudo apt update && sudo apt install -y git zsh tmux neovim curl npm python3-pip ranger
 
 # 2. ZSH setup: place ohmyzsh with plugins (autosuggestions, highlighting, and theme)
 sudo chsh -s `which zsh`
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-pip install pygments
+if [ -z $ZSH ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+ZSH_PLUGINS="$HOME/.oh-my-zsh/custom/plugins"
+if [ ! -d $ZSH_PLUGINS/zsh-autosuggestions ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_PLUGINS/zsh-autosuggestions
+fi
+if [ ! -d $ZSH_PLUGINS/zsh-syntax-highlighting ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_PLUGINS/zsh-syntax-highlighting
+fi
+pip install pygments ipython scipy numpy pandas
 
 # 3. DOTFILE placement: place neovim, tmux, zshrc, zsh_aliases, other in correct config directory
 mkdir -p ~/.config/nvim && cp ./nvim/* ~/.config/nvim/
